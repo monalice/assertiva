872f1c76d3c61bda71053ead8c5564d34f6d6530
Alice Amorim <alice.amorim@engenharia.ufjf.br>
2023-04-28 00:38:53 -0300

    crud básico

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,7 @@
 import express, {json} from "express";
 import dotenv from "dotenv";
 import cors from "cors"
-import connection from './config/database.js';
+import clients from "./utils/querys.js"
 
 dotenv.config({path:"../.env"});
 const app = express();
@@ -9,25 +9,89 @@ const app = express();
 app.use(json());
 app.use(cors());
 
-app.get('/', async (req, res) => {
+app.get('/clientes', async (req, res) => {
     try {
-        const result = await connection.query('SELECT * FROM clients');
-        const results = { results: (result) ? result.rows : null };
-        res.send(results);
-    } catch (err) {
-        console.error(err);
+        const result = await clients.getAllClients();
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-app.post("/cadastro", async (req, res) => {})
+app.get('/clientes/:id', async (req, res) => {
+    try {
+        const result = await clients.getClientById(req.params.id);
+        if (!result) {
+            res.status(404).json({ error: 'Not Found' });
+    } else {
+        res.json(result);
+    }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
-app.get("/busca/ddd", async (req, res) => {})
+app.get('/clientes/filtro/ddd/:ddd', async (req, res) => {
+    try {
 
-app.get("/busca/nome", async (req, res) => {})
+        const result = await clients.filterClientsByCelularDDD(req.params.ddd);
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
-app.put("/editar/:id", async (req, res) => {})
+app.get('/clientes/filtro/nome/:nome', async (req, res) => {
+    try {
+        const result = await clients.filterClientsByNome(req.params.nome);
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
-app.delete("/excluir/:id", async (req, res) => {})
+app.post('/clientes', async (req, res) => {
+    try {
+        const result = await clients.createClient(req.body.cpf, req.body.nome, req.body.celular, req.body.celular2, req.body.email, req.body.email2);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.put('/clientes/:id', async (req, res) => {
+    try {
+        const result = await clients.updateClient(req.params.id, req.body.cpf, req.body.nome, req.body.celular1, req.body.celular2, req.body.email1, req.body.email2);
+        if (!result) {
+            res.status(404).json({ error: 'Not Found' });
+        } else {
+        res.json(result);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.delete('/clientes/:id', async (req, res) => {
+    try {
+        const result = await clients.deleteClient(req.params.id);
+        if (!result) {
+            res.status(404).json({ error: 'Not Found' });
+    } else {
+        res.status(204).json(result);
+    }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 const port = process.env.PORT || 3000;
 const nome = process.env.NOME || "Teste Assertiva";
 
